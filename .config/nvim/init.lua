6b603b5655a7d6bccc9d5c549a2d8f698f7a628b
Olivier Lemoine <olivier@le-moine.fr>
2022-07-22 18:40:36 +0200

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,24 @@
+local paq_path = vim.fn.stdpath('data') .. '/site/paqs/start/paq-nvim'
+
+local function paq_cloned()
+    return vim.fn.empty(vim.fn.glob(paq_path)) == 0
+end
+
+local function clone_paq()
+    vim.fn.system {
+        "git",
+        "clone",
+        "--depth=1",
+        "https://github.com/savq/paq-nvim.git",
+        paq_path
+    }
+end
+
+if not paq_cloned() then
+    clone_paq()
+    vim.cmd('packadd paq-nvim')
+end
+
 require "paq" {
     -- Paq bootstrap
     "savq/paq-nvim",
@@ -27,7 +48,7 @@ require "paq" {
 
     -- LSP
     "neovim/nvim-lspconfig",
-    "williamboman/nvim-lsp-installer",
+--    "williamboman/nvim-lsp-installer",
 
     -- Code
     "nvim-treesitter/nvim-treesitter",
@@ -43,125 +64,145 @@ require "paq" {
 
     -- Terminal
     "akinsho/toggleterm.nvim",
-}
+}:install()
 
-vim.cmd [[
-    call plug#begin("~/.config/nvim/plugged")
-    Plug 'ziglang/zig.vim'
-    Plug 'prettier/vim-prettier'
-    call plug#end()
-]]
+--function printTableKeys(tab)
+--    for k, _ in pairs(tab) do
+--        print(k)
+--    end
+--end
+--printTableKeys(vim.diagnostic)
 
--- Core settings
-vim.g.mapleader = "\\"
-
-vim.o.mouse = "a"
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.autoread = true
-vim.o.backup = false
-vim.o.clipboard = "unnamedplus"
-vim.o.fileencoding = "utf-8"
-vim.o.hlsearch = true
-vim.o.smartindent = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.swapfile = false
-vim.o.updatetime = 300
-vim.o.writebackup = false
-vim.o.cursorline = true
-vim.o.wrap = true
-vim.o.signcolumn = "yes"
-vim.o.completeopt = "menu,menuone,noselect"
-vim.o.termguicolors = true
-
-vim.g.netrw_preview = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 20
-vim.g.netrw_localcopydircmd = "cp -r"
-
--- Remap
-pcall(vim.cmd, "unmap <C-l>")
-
-local vimp = require "vimp"
-
-vimp.nnoremap("<leader>ed", ":e $HOME/.config/nvim/init.lua<CR>")
-vimp.nnoremap("<leader>rr", function()
-    vimp.unmap_all()
-    vim.cmd "silent wa"
-    dofile(vim.fn.stdpath("config") .. "/init.lua")
-    print "Config reloaded."
-end)
-vimp.nnoremap("<leader>rp", function()
-    vim.cmd "PaqSync"
-end)
-
-vimp.inoremap("jj", "<Esc>")
-
-vimp.nnoremap("ä", "\"a")
-vimp.vnoremap("ä", "\"a")
-
-vimp.nnoremap({ "silent" }, "<S-j>", ":bp<CR>")
-vimp.nnoremap({ "silent" }, "<S-k>", ":bn<CR>")
-
-vimp.nnoremap({ "silent" }, "<C-h>", "<C-w>h")
-vimp.nnoremap({ "silent" }, "<C-j>", "<C-w>j")
-vimp.nnoremap({ "silent" }, "<C-k>", "<C-w>k")
-vimp.nnoremap({ "silent" }, "<C-l>", "<C-w>l")
-
-vimp.tnoremap("<Esc>", "<C-\\><C-n>")
-
-vimp.nnoremap({ "silent" }, "<C-w>", ":bd<CR>")
-
-local lazygit = require "toggleterm.terminal".Terminal:new { direction = "float", cmd = "lazygit" }
-vimp.nnoremap("<leader>gi", function() lazygit:toggle() end)
-vimp.nnoremap("<leader>lsp", function() vim.cmd "LspInstallInfo" end)
-vimp.nnoremap("<leader>lsi", function() vim.cmd "LspInfo" end)
-vimp.nnoremap("<C-p>", function() vim.cmd "Telescope live_grep" end)
-vimp.nnoremap({ "silent" }, "<Bar>", function()
-    if vim.bo.filetype == "netrw" then
-        vim.cmd("bd")
-    else
-        vim.cmd("Explore %:p:h")
-    end
-end)
-
-vimp.nnoremap("m", function() vim.lsp.buf.hover() end)
-vimp.nnoremap("gd", function() vim.lsp.buf.definition() end)
-vimp.nnoremap("gi", function() vim.lsp.buf.implementation() end)
-vimp.nnoremap("<leader>a", function() vim.lsp.buf.code_action() end)
-vimp.nnoremap("[g", function() vim.diagnostic.goto_prev() end)
-vimp.nnoremap("]g", function() vim.diagnostic.goto_next() end)
-vimp.nnoremap("<leader>rn", function() vim.lsp.buf.rename() end)
-vimp.nnoremap("<leader>f", function() Format() end)
-
-function printTableKeys(tab)
-    for k, _ in pairs(tab) do
-        print(k)
+function try_setup(setup_name, ...)
+    local ran, error_msg = pcall(...)
+    if not ran then
+        error("Error executing" .. setup_name .. "\n" .. error_msg)
     end
 end
 
---printTableKeys(vim.diagnostic)
+-- Plug
+try_setup("Plug", function ()
+    vim.cmd [[
+        call plug#begin("~/.config/nvim/plugged")
+        Plug 'ziglang/zig.vim'
+        Plug 'prettier/vim-prettier'
+        call plug#end()
+    ]]
+end)
+
+-- Core
+try_setup("Core editor params", function ()
+    vim.g.mapleader = "\\"
+
+    vim.o.mouse = "a"
+    vim.o.number = true
+    vim.o.relativenumber = true
+    vim.o.tabstop = 4
+    vim.o.shiftwidth = 4
+    vim.o.expandtab = true
+    vim.o.autoindent = true
+    vim.o.smartindent = true
+    vim.o.autoread = true
+    vim.o.backup = false
+    vim.o.clipboard = "unnamedplus"
+    vim.o.fileencoding = "utf-8"
+    vim.o.hlsearch = true
+    vim.o.smartindent = true
+    vim.o.splitbelow = true
+    vim.o.splitright = true
+    vim.o.swapfile = false
+    vim.o.updatetime = 300
+    vim.o.writebackup = false
+    vim.o.cursorline = true
+    vim.o.wrap = true
+    vim.o.signcolumn = "yes"
+    vim.o.completeopt = "menu,menuone,noselect"
+    vim.o.termguicolors = true
+
+    vim.g.netrw_preview = 0
+    vim.g.netrw_liststyle = 3
+    vim.g.netrw_keepdir = 0
+    vim.g.netrw_winsize = 20
+    vim.g.netrw_localcopydircmd = "cp -r"
+end)
+
+-- Remap
+local vimp = require "vimp"
+try_setup("Basic keybindings", function()
+    vimp.inoremap("jj", "<Esc>")
+
+    vimp.nnoremap("<leader>ed", ":e $HOME/.config/nvim/init.lua<CR>")
+    vimp.nnoremap("<leader>rr", function()
+        vimp.unmap_all()
+        vim.cmd "silent wa"
+        dofile(vim.fn.stdpath("config") .. "/init.lua")
+        print "Config reloaded."
+    end)
+    vimp.nnoremap("<leader>rp", function()
+        vim.cmd "PaqSync"
+    end)
+
+    vimp.nnoremap({ "silent" }, "<C-w>", ":bd<CR>")
+end)
+
+try_setup("Movement keybindings", function()
+    vimp.nnoremap("ä", "\"a")
+    vimp.vnoremap("ä", "\"a")
+
+    vimp.nnoremap({ "silent" }, "<S-j>", ":bp<CR>")
+    vimp.nnoremap({ "silent" }, "<S-k>", ":bn<CR>")
+
+    pcall(vim.cmd, "unmap <C-l>")
+    vimp.nnoremap({ "silent" }, "<C-h>", "<C-w>h")
+    vimp.nnoremap({ "silent" }, "<C-j>", "<C-w>j")
+    vimp.nnoremap({ "silent" }, "<C-k>", "<C-w>k")
+    vimp.nnoremap({ "silent" }, "<C-l>", "<C-w>l")
+end)
+
+try_setup("Utils keybindings", function()
+    vimp.tnoremap("<Esc>", "<C-\\><C-n>")
+
+    local lazygit = require "toggleterm.terminal".Terminal:new { direction = "float", cmd = "lazygit" }
+    vimp.nnoremap("<leader>gi", function() lazygit:toggle() end)
+    vimp.nnoremap("<C-p>", function() vim.cmd "Telescope live_grep" end)
+    vimp.nnoremap({ "silent" }, "<Bar>", function()
+        if vim.bo.filetype == "netrw" then
+            vim.cmd("bd")
+        else
+            vim.cmd("Explore %:p:h")
+        end
+    end)
+end)
+
+try_setup("LSP keybindings", function()
+    vimp.nnoremap("<leader>lsp", function() vim.cmd "LspInstallInfo" end)
+    vimp.nnoremap("<leader>lsi", function() vim.cmd "LspInfo" end)
+
+    vimp.nnoremap("m", function() vim.lsp.buf.hover() end)
+    vimp.nnoremap("gd", function() vim.lsp.buf.definition() end)
+    vimp.nnoremap("gi", function() vim.lsp.buf.implementation() end)
+    vimp.nnoremap("<leader>a", function() vim.lsp.buf.code_action() end)
+    vimp.nnoremap("[g", function() vim.diagnostic.goto_prev() end)
+    vimp.nnoremap("]g", function() vim.diagnostic.goto_next() end)
+    vimp.nnoremap("<leader>rn", function() vim.lsp.buf.rename() end)
+    vimp.nnoremap("<leader>f", function() Format() end)
+end)
 
 -- Theme
-vim.g.oceanic_next_terminal_bold = 1
-vim.g.oceanic_next_terminal_italic = 1
-vim.cmd("colorscheme OceanicNext")
+try_setup("Theming", function()
+    vim.g.oceanic_next_terminal_bold = 1
+    vim.g.oceanic_next_terminal_italic = 1
+    vim.cmd("colorscheme OceanicNext")
 
-vim.g.airline_powerline_fonts = 1
-require "bufferline".setup {
-    options = {
-        numbers = "buffer_id",
-        indicator_icon = "❯",
-        diagnostics = "nvim_lsp",
-    },
-}
+    vim.g.airline_powerline_fonts = 1
+    require "bufferline".setup {
+        options = {
+            numbers = "buffer_id",
+            indicator_icon = "❯",
+            diagnostics = "nvim_lsp",
+        },
+    }
+end)
 
 -- LSP
 local check_backspace = function()
@@ -232,52 +273,18 @@ local util = require "lspconfig/util"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require "cmp_nvim_lsp".update_capabilities(capabilities)
 
-require "lspconfig".hls.setup {
-    settings = {
-        haskell = {
-            formattingProvider = "fourmolu"
-        }
-    }
-}
+--require "nvim-lsp-installer".setup {}
 
-require "nvim-lsp-installer".on_server_ready(function(server)
-    local opts = {
-        capabilities = capabilities,
-    }
+local lspconfig = require "lspconfig"
+try_setup("LSP config: tsserver", function() lspconfig.tsserver.setup {} end)
+try_setup("LSP config: tailwindcss", function() lspconfig.tailwindcss.setup {} end)
+try_setup("LSP config: rust-analyzer", function() lspconfig.rust_analyzer.setup {
+        settings = { ["rust-analyzer"] = { checkOnSave = { AllTargets = false } } }
+} end)
 
-    if server.name == "emmet_ls" then
-        opts.filetypes = {
-            "html",
-            "css",
-            --"javascriptreact",
-            --"javascript.jsx",
-            --"typescript",
-            --"typescriptreact",
-            --"typescript.tsx",
-        }
-    end
-
-    if server.name == "zls" then
-        opts = {
-            cmd = { "zls" },
-            filetypes = { "zig" },
-            root_dir = util.root_pattern({ "build.zig", ".git" }),
-            single_file_support = true,
-        }
-    end
-
-    if server.name == "rust_analyzer" then
-        opts.settings = {
-            ["rust-analyzer"] = {
-                checkOnSave = {
-                    AllTargets = false
-                }
-            }
-        }
-    end
-
-    server:setup(opts)
-end)
+--lspconfig.hls.setup {
+--    settings = { haskell = { formattingProvider = "fourmolu" } }
+--}
 
 require "nvim-treesitter.configs".setup {
     highlight = {
@@ -291,8 +298,8 @@ function Format()
     vim.lsp.buf.formatting_sync()
 end
 
---vim.cmd "autocmd BufWritePre *.tsx Prettier"
 vim.cmd "autocmd BufWritePre <buffer> lua Format()"
+vim.cmd "autocmd BufWritePre *.tsx Prettier"
 
 -- Fuzzy finding
 require('telescope').setup {
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 DIR=$(cd $(dirname $0) && pwd)
 
+rm -rf $HOME/.config/
+rm -rf $HOME/.tmux.conf
+
 ln -s $DIR/.config $HOME/
 ln -s $DIR/.tmux.conf $HOME/
